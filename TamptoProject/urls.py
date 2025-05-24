5a5d61d92413c9a262338b056e86aade07bcb087
square025 <mahadwangstar@gmail.com>
2025-05-25 01:55:47 +0300

    update1

--- TamptoProject/urls.py
+++ TamptoProject/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("https://tampto.onrender.com", views.index, name="index"),
-    path('https://tampto.onrender.com/register/', views.register, name='register'),
-    path('https://tampto.onrender.com/contribution/', views.contribution, name='contribution'),
-    path('https://tampto.onrender.com/registration/', views.registration, name='registration'),
+    path("", views.index, name="index"),
+    path('register/', views.register, name='register'),
+    path('contribution/', views.contribution, name='contribution'),
+    path('registration/', views.registration, name='registration'),
 ]
--- tampro/settings.py
+++ tampro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-vd76ibw8db_4b*i*tjo!8um8-z&ldx!8)zjv9ktksmh$$juw(@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tampto.onrender.com']
 
 
 # Application definition
